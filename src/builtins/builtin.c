71b93850e021c32d291e6370d1043b9b4d9979e8
José Beirão <github@zbeirao.com>
2025-06-03 20:51:49 +0100

    awda

--- src/builtins/builtin.c
+++ src/builtins/builtin.c
@@ -6,7 +6,7 @@
 /*   By: joseferr <joseferr@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 14:27:48 by joseferr          #+#    #+#             */
-/*   Updated: 2025/06/02 22:15:15 by joseferr         ###   ########.fr       */
+/*   Updated: 2025/06/03 20:51:44 by joseferr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,6 @@ void	ft_execute_builtin(t_data *data, char **cmd_args)
 	cmd = cmd_args[0];
 	len = ft_strlen(cmd) + 1;
 	data->status = 0;
-	printf("Executing builtin\n");
 	if (!ft_strncmp(OP_EXT, cmd, len))
 		ft_exit(data, cmd_args);
 	if (!ft_strncmp(OP_PWD, cmd, len))
